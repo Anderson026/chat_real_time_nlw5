93d08fef1ea6baaf0f13976a04c4efc63ed95520
Anderson026 <anderson-ferreira22@hotmail.com>
2021-04-20 10:33:10 -0300

    Criando, configurando a primeira migration e sua entitie

---
+++ src/database/migrations/1618921936766-CreateSettings.ts
@@ -0,0 +1,47 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSettings1618921936766 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // criando a tabela settings
+        await queryRunner.createTable(
+            /* aqui se configura os dados que terá na tabela como id, uuid
+             (id universal), primary key, entre outros*/
+            new Table({
+                name: "settings",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "username",
+                        type: "varchar"
+                    },
+                    {
+                        name: "chat",
+                        type: "boolean",
+                        default: true
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // desfazer o que foi feito no up
+        await queryRunner.dropTable("settings");
+    }
+
+}
---
+++ src/entities/Setting.ts
@@ -0,0 +1,31 @@
+// importando as entidades, colunas, criar e mudar colunas
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn } from "typeorm"
+// importando o uuid
+import { v4 as uuid } from "uuid"
+
+// criando a classe com os dados referentes a tabela Settings
+@Entity("settings")
+class Setting {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  usermane: string;
+
+  @Column()
+  chat: boolean;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+  // verifica se o id ta preenchido, se não tiver preenche o id
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+// exportando a classe Settings
+export { Setting };
