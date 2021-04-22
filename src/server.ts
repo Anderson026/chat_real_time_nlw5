738e5e241d60868d86bed7a844b35053025b727b
Anderson026 <anderson-ferreira22@hotmail.com>
2021-04-22 10:15:39 -0300

    Iniciando o dia 4, adicionando a pasta public com o frontend do projeto e iniciando a configuraçao do web socket

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 // importando o express
 import express from "express";
+// importando o servidor padrão do node para criar o servidor web socket
+import { createServer } from "http";
+// importando o server e o socket io
+import { Server, Socket } from "socket.io";
+// importando o path
+import path from "path";
 // importando o banco de dados
 import "./database";
 // importando as rotas
@@ -7,6 +13,28 @@ import { routes } from "./routes";
 
 // colocando o express na variável app
 const app = express();
+// define onde estão os arquivos estáticos
+app.use(express.static(path.join(__dirname, "..", "public")));
+// define onde as views estão na pasta public
+app.set("views", path.join(__dirname, "..","public"));
+// define a view engine
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+// rota de teste
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+// criando o protocolo http
+const http = createServer(app);
+// criando o servidor Web Socket
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
+
 // configurando o json no express
 app.use(express.json());
 
