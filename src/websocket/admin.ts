48896929c053abb989ef9a7c1991cbf686d2ef44
Anderson026 <anderson-ferreira22@hotmail.com>
2021-04-23 10:58:10 -0300

    Criando o evento para ter acesso as mensagens do usuario na tela do admin

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -2,11 +2,25 @@
 import { io } from "../http";
 // importando o connectionsService
 import { ConnectionsService } from "../services/ConnectionsService";
+// importando o messagesService
+import { MessagesService } from "../services/MessagesService";
+
+
 // listagem das conexões
 io.on("connect", async socket => {
   const connectionsService = new ConnectionsService();
+  const messagesService = new MessagesService();
 
   const  allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
   // emite o evento de listagem de mensagens 
   io.emit("admin_list_all_users", allConnectionsWithoutAdmin)
-})
+
+  // criando o evento que vai mostrar todas as mensagens do usuário
+  socket.on("admin_list_messages_by_user", async (params, callback) => {
+    const { user_id } = params;
+
+    const allMessages = await messagesService.listByUser(user_id)
+
+    callback(allMessages);
+  })
+});
